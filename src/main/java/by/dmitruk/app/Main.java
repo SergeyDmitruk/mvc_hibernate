252e52579c6413123221dccc452494bae6e0c880
Sergey <sergey.dmitruk.01@mail.ru>
2023-04-17 13:19:00 +0300

    add spring boot

---
+++ src/main/java/by/dmitruk/app/Main.java
@@ -0,0 +1,11 @@
+package by.dmitruk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
